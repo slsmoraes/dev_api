4e3f701e45a43ff5e383dd971a5deb1daaf417a3
slsmoraes <slsmoraes@gmail.com>
2020-10-27 21:27:13 -0300

    D20201027H2126-Incluido requirements do projeto, o app_restful e o flask-restful e o modulo de habilidades

---
+++ app_restful.py
@@ -0,0 +1,58 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+import json
+from habilidades import Habilidades
+
+app = Flask(__name__)
+api = Api(app)
+
+desenvolvedores = [
+    {'id': '0',
+     'nome':'Rafael',
+     'habilidades':['Python', 'Flask']},
+    {'id':'1',
+     'nome':'Galiane',
+     'habilidades':['Python', 'Django']}
+]
+
+class Desenvolvedor(Resource):
+    # Devolve um desenvolvedor pelo ID, tb altera e deleta em desenvolvedor
+    def get(self,id):
+        try:
+            response = desenvolvedores[id]
+        except IndexError:
+            mensagem = 'Desenvolvedor de Id {} não existe'.format(id)
+            response = {'status': 'erro', 'mensagem': mensagem}
+        except Exception:
+            mensagem = 'Erro desconhecido. Procure o administrador da API'
+            response = {'status': 'erro', 'mensagem': mensagem}
+        return response
+
+    def put(self,id):
+        dados = json.loads(request.data)
+        desenvolvedores[id] = dados
+        return (dados)
+
+    def delete(self,id):
+        desenvolvedores.pop(id)
+        return ({'status': 'sucesso', 'mensagem': 'Registro excluído!!!'})
+
+#Lista todos os desenvolvedores e permite incluir um novo.
+class ListaDesenvolvedores(Resource):
+    def get(self):
+        return desenvolvedores
+
+    def post(self):
+        dados = json.loads(request.data)
+        posicao = len(desenvolvedores)
+        dados['id'] = posicao
+        desenvolvedores.append(dados)
+        return (desenvolvedores[posicao])
+
+
+api.add_resource(Desenvolvedor, '/dev/<int:id>/')
+api.add_resource(ListaDesenvolvedores, '/dev/')
+api.add_resource(Habilidades, '/habilidades/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
